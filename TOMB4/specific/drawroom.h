d81d2825ec1c7dbf034c7f1a30938738713e4afd
Troye <woopstombraider@gmail.com>
2022-01-24 13:18:38 -0600

    PrelightVertsNonMMX(); InsertRoom();

--- TOMB4/specific/drawroom.h
+++ TOMB4/specific/drawroom.h
@@ -7,6 +7,9 @@ void ProjectVerts(long nVerts, D3DTLVERTEX* v, short* clip);
 void ProjectWaterVerts(long nVerts, D3DTLVERTEX* v, short* clip);
 void ProjectShoreVerts(long nVerts, D3DTLVERTEX* v, short* clip);
 void ProcessRoomData(ROOM_INFO* r);
+void PrelightVertsNonMMX(long nVerts, D3DTLVERTEX* v, ROOM_INFO* r);
+void InsertRoom(ROOM_INFO* r);
 
 #define InitBuckets	( (void(__cdecl*)()) 0x004728B0 )
 #define CreateVertexNormals	( (void(__cdecl*)(ROOM_INFO*)) 0x00472F50 )
+#define PrelightVertsMMX	( (void(__cdecl*)(long, D3DTLVERTEX*, ROOM_INFO*)) 0x00472400 )
