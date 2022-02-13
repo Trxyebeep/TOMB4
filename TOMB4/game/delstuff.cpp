7d2f14ca95be55d27630837cc5bd11cc3e3b597d
Troye <woopstombraider@gmail.com>
2022-02-13 10:50:13 -0600

    Rich_CalcLaraMatrices_Interpolated();

--- TOMB4/game/delstuff.cpp
+++ TOMB4/game/delstuff.cpp
@@ -689,10 +689,359 @@ void Rich_CalcLaraMatrices_Normal(short* frame, long* bone, long flag)
 	GLaraShadowframe = frame;
 }
 
+void Rich_CalcLaraMatrices_Interpolated(short* frame1, short* frame2, long frac, long rate, long* bone, long flag)
+{
+	PHD_VECTOR vec;
+	long* matrix;
+	long* arms;
+	short* rot;
+	short* rot2;
+	short* rotcopy;
+	short* rot2copy;
+	short gun;
+
+	if (flag == 1)
+		matrix = lara_joint_matrices;
+	else
+		matrix = lara_matrices;
+
+	phd_PushMatrix();
+
+	if (!flag || flag == 2)
+		phd_TranslateAbs(lara_item->pos.x_pos, lara_item->pos.y_pos, lara_item->pos.z_pos);
+	else
+	{
+		phd_mxptr[M03] = 0;
+		phd_mxptr[M13] = 0;
+		phd_mxptr[M23] = 0;
+	}
+
+	phd_RotYXZ(lara_item->pos.y_rot, lara_item->pos.x_rot, lara_item->pos.z_rot);
+
+	if (flag == 2)
+	{
+		vec.x = -16384;
+		vec.y = -16384;
+		vec.z = -16384;
+		ScaleCurrentMatrix(&vec);
+	}
+
+	arms = phd_mxptr;
+	phd_PushMatrix();
+	rot = frame1 + 9;
+	rot2 = frame2 + 9;
+	InitInterpolate(frac, rate);
+	phd_TranslateRel_ID(frame1[6], frame1[7], frame1[8], frame2[6], frame2[7], frame2[8]);
+	gar_RotYXZsuperpack_I(&rot, &rot2, 0);
+
+	phd_PushMatrix();
+	InterpolateMatrix();
+	memcpy(matrix, phd_mxptr, 48);
+	matrix += 12;
+	phd_PopMatrix();
+
+	phd_PushMatrix_I();
+	phd_TranslateRel_I(bone[1], bone[2], bone[3]);
+	gar_RotYXZsuperpack_I(&rot, &rot2, 0);
+	phd_PushMatrix();
+	InterpolateMatrix();
+	memcpy(matrix, phd_mxptr, 48);
+	matrix += 12;
+	phd_PopMatrix();
+
+	phd_TranslateRel_I(bone[5], bone[6], bone[7]);
+	gar_RotYXZsuperpack_I(&rot, &rot2, 0);
+	phd_PushMatrix();
+	InterpolateMatrix();
+	memcpy(matrix, phd_mxptr, 48);
+	matrix += 12;
+	phd_PopMatrix();
+
+	phd_TranslateRel_I(bone[9], bone[10], bone[11]);
+	gar_RotYXZsuperpack_I(&rot, &rot2, 0);
+	phd_PushMatrix();
+	InterpolateMatrix();
+	memcpy(matrix, phd_mxptr, 48);
+	matrix += 12;
+	phd_PopMatrix();
+	phd_PopMatrix_I();
+
+	phd_PushMatrix_I();
+	phd_TranslateRel_I(bone[13], bone[14], bone[15]);
+	gar_RotYXZsuperpack_I(&rot, &rot2, 0);
+	phd_PushMatrix();
+	InterpolateMatrix();
+	memcpy(matrix, phd_mxptr, 48);
+	matrix += 12;
+	phd_PopMatrix();
+
+	phd_TranslateRel_I(bone[17], bone[18], bone[19]);
+	gar_RotYXZsuperpack_I(&rot, &rot2, 0);
+	phd_PushMatrix();
+	InterpolateMatrix();
+	memcpy(matrix, phd_mxptr, 48);
+	matrix += 12;
+	phd_PopMatrix();
+
+	phd_TranslateRel_I(bone[21], bone[22], bone[23]);
+	gar_RotYXZsuperpack_I(&rot, &rot2, 0);
+	phd_PushMatrix();
+	InterpolateMatrix();
+	memcpy(matrix, phd_mxptr, 48);
+	matrix += 12;
+	phd_PopMatrix();
+	phd_PopMatrix_I();
+
+	phd_TranslateRel_I(bone[25], bone[26], bone[27]);
+	gar_RotYXZsuperpack_I(&rot, &rot2, 0);
+	phd_RotYXZ_I(lara.torso_y_rot, lara.torso_x_rot, lara.torso_z_rot);
+	phd_PushMatrix();
+	InterpolateMatrix();
+	memcpy(matrix, phd_mxptr, 48);
+	matrix += 12;
+	phd_PopMatrix();
+
+	phd_PushMatrix_I();
+	phd_TranslateRel_I(bone[53], bone[54], bone[55]);
+	rotcopy = rot;
+	rot2copy = rot2;
+	gar_RotYXZsuperpack_I(&rotcopy, &rot2copy, 6);
+	phd_RotYXZ_I(lara.head_y_rot, lara.head_x_rot, lara.head_z_rot);
+	phd_PushMatrix();
+	InterpolateMatrix();
+	memcpy(matrix, phd_mxptr, 48);
+	matrix += 12;
+	phd_PopMatrix();
+	phd_PopMatrix_I();
+
+	gun = WEAPON_NONE;
+
+	if (lara.gun_status == LG_READY || lara.gun_status == LG_FLARE || lara.gun_status == LG_DRAW_GUNS || lara.gun_status == LG_UNDRAW_GUNS)
+		gun = lara.gun_type;
+
+	switch (gun)
+	{
+	case WEAPON_NONE:
+	case WEAPON_FLARE:
+	case WEAPON_TORCH:
+		phd_PushMatrix_I();
+		phd_TranslateRel_I(bone[29], bone[30], bone[31]);
+		gar_RotYXZsuperpack_I(&rot, &rot2, 0);
+		phd_PushMatrix();
+		InterpolateMatrix();
+		memcpy(matrix, phd_mxptr, 48);
+		matrix += 12;
+		phd_PopMatrix();
+
+		phd_TranslateRel_I(bone[33], bone[34], bone[35]);
+		gar_RotYXZsuperpack_I(&rot, &rot2, 0);
+		phd_PushMatrix();
+		InterpolateMatrix();
+		memcpy(matrix, phd_mxptr, 48);
+		matrix += 12;
+		phd_PopMatrix();
+
+		phd_TranslateRel_I(bone[37], bone[38], bone[39]);
+		gar_RotYXZsuperpack_I(&rot, &rot2, 0);
+		phd_PushMatrix();
+		InterpolateMatrix();
+		memcpy(matrix, phd_mxptr, 48);
+		matrix += 12;
+		phd_PopMatrix();
+		phd_PopMatrix_I();
+
+		phd_PushMatrix_I();
+		phd_TranslateRel_I(bone[41], bone[42], bone[43]);
+
+		if (lara.flare_control_left)
+		{
+			rot2 = &lara.left_arm.frame_base[(anims[lara.left_arm.anim_number].interpolation >> 8) *
+				(lara.left_arm.frame_number - anims[lara.left_arm.anim_number].frame_base) + 9];
+			rot = rot2;
+			gar_RotYXZsuperpack_I(&rot, &rot2, 11);
+		}
+		else
+			gar_RotYXZsuperpack_I(&rot, &rot2, 0);
+
+		phd_PushMatrix();
+		InterpolateMatrix();
+		memcpy(matrix, phd_mxptr, 48);
+		matrix += 12;
+		phd_PopMatrix();
+
+		phd_TranslateRel_I(bone[45], bone[46], bone[47]);
+		gar_RotYXZsuperpack_I(&rot, &rot2, 0);
+		phd_PushMatrix();
+		InterpolateMatrix();
+		memcpy(matrix, phd_mxptr, 48);
+		matrix += 12;
+		phd_PopMatrix();
+
+		phd_TranslateRel_I(bone[49], bone[50], bone[51]);
+		gar_RotYXZsuperpack_I(&rot, &rot2, 0);
+		phd_PushMatrix();
+		InterpolateMatrix();
+		memcpy(matrix, phd_mxptr, 48);
+		matrix += 12;
+		phd_PopMatrix();
+		break;
+
+	case WEAPON_PISTOLS:
+	case WEAPON_UZI:
+		phd_PushMatrix_I();
+		phd_TranslateRel_I(bone[29], bone[30], bone[31]);
+		InterpolateArmMatrix(arms);
+		phd_RotYXZ(lara.right_arm.y_rot, lara.right_arm.x_rot, lara.right_arm.z_rot);
+		rot = &lara.right_arm.frame_base[(anims[lara.right_arm.anim_number].interpolation >> 8) *
+			(lara.right_arm.frame_number - anims[lara.right_arm.anim_number].frame_base) + 9];
+		gar_RotYXZsuperpack(&rot, 8);
+		memcpy(matrix, phd_mxptr, 48);
+		matrix += 12;
+		
+		phd_TranslateRel(bone[33], bone[34], bone[35]);
+		gar_RotYXZsuperpack(&rot, 0);
+		memcpy(matrix, phd_mxptr, 48);
+		matrix += 12;
+		
+		phd_TranslateRel(bone[37], bone[38], bone[39]);
+		gar_RotYXZsuperpack(&rot, 0);
+		memcpy(matrix, phd_mxptr, 48);
+		matrix += 12;
+		phd_PopMatrix_I();
+
+		phd_PushMatrix_I();
+		phd_TranslateRel_I(bone[41], bone[42], bone[43]);
+		InterpolateArmMatrix(arms);
+		phd_RotYXZ(lara.left_arm.y_rot, lara.left_arm.x_rot, lara.left_arm.z_rot);
+		rot = &lara.left_arm.frame_base[(anims[lara.left_arm.anim_number].interpolation >> 8) *
+			(lara.left_arm.frame_number - anims[lara.left_arm.anim_number].frame_base) + 9];
+		gar_RotYXZsuperpack(&rot, 11);
+		memcpy(matrix, phd_mxptr, 48);
+		matrix += 12;
+
+		phd_TranslateRel(bone[45], bone[46], bone[47]);
+		gar_RotYXZsuperpack(&rot, 0);
+		memcpy(matrix, phd_mxptr, 48);
+		matrix += 12;
+
+		phd_TranslateRel(bone[49], bone[50], bone[51]);
+		gar_RotYXZsuperpack(&rot, 0);
+		memcpy(matrix, phd_mxptr, 48);
+		matrix += 12;
+		break;
+
+	case WEAPON_REVOLVER:
+		phd_PushMatrix_I();
+		phd_TranslateRel_I(bone[29], bone[30], bone[31]);
+		InterpolateArmMatrix(arms);
+		phd_RotYXZ_I(lara.torso_y_rot, lara.torso_x_rot, lara.torso_z_rot);
+		rot = &lara.right_arm.frame_base[(anims[lara.right_arm.anim_number].interpolation >> 8) *
+			(lara.right_arm.frame_number - anims[lara.right_arm.anim_number].frame_base) + 9];
+		gar_RotYXZsuperpack(&rot, 8);
+		memcpy(matrix, phd_mxptr, 48);
+		matrix += 12;
+
+		phd_TranslateRel(bone[33], bone[34], bone[35]);
+		gar_RotYXZsuperpack(&rot, 0);
+		memcpy(matrix, phd_mxptr, 48);
+		matrix += 12;
+
+		phd_TranslateRel(bone[37], bone[38], bone[39]);
+		gar_RotYXZsuperpack(&rot, 0);
+		memcpy(matrix, phd_mxptr, 48);
+		matrix += 12;
+		phd_PopMatrix_I();
+
+		phd_PushMatrix_I();
+		phd_TranslateRel_I(bone[41], bone[42], bone[43]);
+		InterpolateArmMatrix(arms);
+		phd_RotYXZ_I(lara.torso_y_rot, lara.torso_x_rot, lara.torso_z_rot);
+		rot = &lara.left_arm.frame_base[(anims[lara.left_arm.anim_number].interpolation >> 8) *
+			(lara.left_arm.frame_number - anims[lara.left_arm.anim_number].frame_base) + 9];
+		gar_RotYXZsuperpack(&rot, 11);
+		memcpy(matrix, phd_mxptr, 48);
+		matrix += 12;
+
+		phd_TranslateRel(bone[45], bone[46], bone[47]);
+		gar_RotYXZsuperpack(&rot, 0);
+		memcpy(matrix, phd_mxptr, 48);
+		matrix += 12;
+
+		phd_TranslateRel(bone[49], bone[50], bone[51]);
+		gar_RotYXZsuperpack(&rot, 0);
+		memcpy(matrix, phd_mxptr, 48);
+		matrix += 12;
+		break;
+
+	case WEAPON_SHOTGUN:
+	case WEAPON_GRENADE:
+	case WEAPON_CROSSBOW:
+		phd_PushMatrix_I();
+		phd_TranslateRel_I(bone[29], bone[30], bone[31]);
+		rot2 = &lara.right_arm.frame_base[lara.right_arm.frame_number *
+			(anims[lara.right_arm.anim_number].interpolation >> 8) + 9];
+		rot = rot2;
+		gar_RotYXZsuperpack_I(&rot, &rot2, 8);
+		phd_PushMatrix();
+		InterpolateMatrix();
+		memcpy(matrix, phd_mxptr, 48);
+		matrix += 12;
+		phd_PopMatrix();
+
+		phd_TranslateRel_I(bone[33], bone[34], bone[35]);
+		gar_RotYXZsuperpack_I(&rot, &rot2, 0);
+		phd_PushMatrix();
+		InterpolateMatrix();
+		memcpy(matrix, phd_mxptr, 48);
+		matrix += 12;
+		phd_PopMatrix();
+
+		phd_TranslateRel_I(bone[37], bone[38], bone[39]);
+		gar_RotYXZsuperpack_I(&rot, &rot2, 0);
+		phd_PushMatrix();
+		InterpolateMatrix();
+		memcpy(matrix, phd_mxptr, 48);
+		matrix += 12;
+		phd_PopMatrix();
+		phd_PopMatrix_I();
+
+		phd_PushMatrix_I();
+		phd_TranslateRel_I(bone[41], bone[42], bone[43]);
+		gar_RotYXZsuperpack_I(&rot, &rot2, 0);
+		phd_PushMatrix();
+		InterpolateMatrix();
+		memcpy(matrix, phd_mxptr, 48);
+		matrix += 12;
+		phd_PopMatrix();
+
+		phd_TranslateRel_I(bone[45], bone[46], bone[47]);
+		gar_RotYXZsuperpack_I(&rot, &rot2, 0);
+		phd_PushMatrix();
+		InterpolateMatrix();
+		memcpy(matrix, phd_mxptr, 48);
+		matrix += 12;
+		phd_PopMatrix();
+
+		phd_TranslateRel_I(bone[49], bone[50], bone[51]);
+		gar_RotYXZsuperpack_I(&rot, &rot2, 0);
+		phd_PushMatrix();
+		InterpolateMatrix();
+		memcpy(matrix, phd_mxptr, 48);
+		matrix += 12;
+		phd_PopMatrix();
+		break;
+	}
+
+	phd_PopMatrix();
+	phd_PopMatrix();
+	phd_PopMatrix();
+}
+
 void inject_delstuff(bool replace)
 {
 	INJECT(0x0041D140, DrawLara, replace);
 	INJECT(0x0041D9A0, GetLaraJointPos, replace);
 	INJECT(0x0041F260, SetLaraUnderwaterNodes, replace);
 	INJECT(0x0041DAF0, Rich_CalcLaraMatrices_Normal, replace);
+	INJECT(0x0041E630, Rich_CalcLaraMatrices_Interpolated, replace);
 }
--- TOMB4/game/delstuff.h
+++ TOMB4/game/delstuff.h
@@ -7,3 +7,4 @@ void DrawLara(ITEM_INFO* item, long mirror);
 void GetLaraJointPos(PHD_VECTOR* pos, long node);
 void SetLaraUnderwaterNodes();
 void Rich_CalcLaraMatrices_Normal(short* frame, long* bone, long flag);
+void Rich_CalcLaraMatrices_Interpolated(short* frame1, short* frame2, long frac, long rate, long* bone, long flag);
