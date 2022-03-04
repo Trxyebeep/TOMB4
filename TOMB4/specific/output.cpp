908e168d92d4c678e3e9ff59abb5860b9fd3163b
Troye <woopstombraider@gmail.com>
2022-03-04 15:32:38 -0600

    phd_PutPolygonsPickup();

--- TOMB4/specific/output.cpp
+++ TOMB4/specific/output.cpp
@@ -500,6 +500,211 @@ void S_InitialisePolyList()
 	InitialiseSortList();
 }
 
+void phd_PutPolygonsPickup(short* objptr, float x, float y, long color)
+{
+	MESH_DATA* mesh;
+	SPRITESTRUCT* envmap_sprite;
+	D3DTLVERTEX* v;
+	TEXTURESTRUCT* pTex;
+	D3DVECTOR normals[4];
+	TEXTURESTRUCT envmap_texture;
+	short* quad;
+	short* tri;
+	float fcx, fcy;
+	long clrbak[4];
+	long spcbak[4];
+	long num;
+	ushort drawbak;
+	bool envmap;
+
+	bWaterEffect = 0;
+	SetD3DViewMatrix();
+	mesh = (MESH_DATA*)objptr;
+	envmap_sprite = &spriteinfo[objects[DEFAULT_SPRITES].mesh_index + 11];
+	ClearDynamicLighting();
+	ClearObjectLighting();
+	App.dx.lpD3DDevice->SetLightState(D3DLIGHTSTATE_AMBIENT, color);
+
+	if (mesh->nVerts)
+		DXAttempt(DestVB->ProcessVertices(D3DVOP_TRANSFORM | D3DVOP_LIGHT, 0, mesh->nVerts, mesh->SourceVB, 0, App.dx._lpD3DDevice, 0));
+
+	DestVB->Lock(DDLOCK_READONLY, (void**)&v, 0);
+	clip_left = f_left;
+	clip_top = f_top;
+	clip_right = f_right;
+	clip_bottom = f_bottom;
+	fcx = f_centerx;
+	fcy = f_centery;
+	f_centerx = x;
+	f_centery = y;
+
+	if (mesh->nVerts)
+	{
+		ProjectVerts(mesh->nVerts, v, clipflags);
+
+		if (mesh->prelight)
+			PrelightVerts(mesh->nVerts, v, mesh);
+		else
+			CalcVertsColorSplitMMX(mesh->nVerts, v);
+	}
+
+	f_centerx = fcx;
+	f_centery = fcy;
+	quad = mesh->gt4;
+
+	for (int i = 0; i < mesh->ngt4; i++, quad += 6)
+	{
+		pTex = &textinfo[quad[4] & 0x7FFF];
+		envmap = 0;
+		drawbak = pTex->drawtype;
+
+		if (quad[5] & 1)
+			pTex->drawtype = 2;
+
+		if (quad[5] & 2)
+		{
+			envmap = 1;
+			num = (quad[5] >> 2) & 0x1F;
+			normals[0] = mesh->Normals[quad[0]];
+			normals[1] = mesh->Normals[quad[1]];
+			normals[2] = mesh->Normals[quad[2]];
+			normals[3] = mesh->Normals[quad[3]];
+			D3DTransform(&normals[0], &D3DMView);
+			D3DTransform(&normals[1], &D3DMView);
+			D3DTransform(&normals[2], &D3DMView);
+			D3DTransform(&normals[3], &D3DMView);
+
+			for (int i = 0; i < 4; i++)
+			{
+				normals[i].x *= 0.125F;
+				normals[i].y *= 0.125F;
+				normals[i].z *= 0.125F;
+			}
+
+			envmap_texture.drawtype = 2;
+			envmap_texture.flag = pTex->flag;
+			envmap_texture.tpage = envmap_sprite->tpage;
+			envmap_texture.u1 = normals[0].x + envmap_sprite->x1 + 0.125F;
+			envmap_texture.v1 = normals[0].y + envmap_sprite->y1 + 0.125F;
+			envmap_texture.u2 = normals[1].x + envmap_sprite->x1 + 0.125F;
+			envmap_texture.v2 = normals[1].y + envmap_sprite->y1 + 0.125F;
+			envmap_texture.u3 = normals[2].x + envmap_sprite->x1 + 0.125F;
+			envmap_texture.v3 = normals[2].y + envmap_sprite->y1 + 0.125F;
+			envmap_texture.u4 = normals[3].x + envmap_sprite->x1 + 0.125F;
+			envmap_texture.v4 = normals[3].y + envmap_sprite->y1 + 0.125F;
+		}
+
+		if (GlobalAlpha != 0xFF000000)
+			pTex->drawtype = 3;
+
+		AddQuadSorted(v, quad[0], quad[1], quad[2], quad[3], pTex, 0);
+
+		if (envmap)
+		{
+			clrbak[0] = v[quad[0]].color;
+			clrbak[1] = v[quad[1]].color;
+			clrbak[2] = v[quad[2]].color;
+			clrbak[3] = v[quad[3]].color;
+			spcbak[0] = v[quad[0]].specular;
+			spcbak[1] = v[quad[1]].specular;
+			spcbak[2] = v[quad[2]].specular;
+			spcbak[3] = v[quad[3]].specular;
+			RGB_M(v[quad[0]].color, num);
+			RGB_M(v[quad[1]].color, num);
+			RGB_M(v[quad[2]].color, num);
+			RGB_M(v[quad[3]].color, num);
+			RGB_M(v[quad[0]].specular, num);
+			RGB_M(v[quad[1]].specular, num);
+			RGB_M(v[quad[2]].specular, num);
+			RGB_M(v[quad[3]].specular, num);
+			AddQuadSorted(v, quad[0], quad[1], quad[2], quad[3], &envmap_texture, 0);
+			v[quad[0]].color = clrbak[0];
+			v[quad[1]].color = clrbak[1];
+			v[quad[2]].color = clrbak[2];
+			v[quad[3]].color = clrbak[3];
+			v[quad[0]].specular = spcbak[0];
+			v[quad[1]].specular = spcbak[1];
+			v[quad[2]].specular = spcbak[2];
+			v[quad[3]].specular = spcbak[3];
+		}
+
+		pTex->drawtype = drawbak;
+	}
+
+	tri = mesh->gt3;
+
+	for (int i = 0; i < mesh->ngt3; i++, tri += 5)
+	{
+		pTex = &textinfo[tri[3] & 0x7FFF];
+		envmap = 0;
+		drawbak = pTex->drawtype;
+
+		if (tri[4] & 1)
+			pTex->drawtype = 2;
+
+		if (tri[4] & 2)
+		{
+			envmap = 1;
+			num = (tri[4] >> 2) & 0x1F;
+			normals[0] = mesh->Normals[tri[0]];
+			normals[1] = mesh->Normals[tri[1]];
+			normals[2] = mesh->Normals[tri[2]];
+			D3DTransform(&normals[0], &D3DMView);
+			D3DTransform(&normals[1], &D3DMView);
+			D3DTransform(&normals[2], &D3DMView);
+
+			for (int i = 0; i < 3; i++)
+			{
+				normals[i].x *= 0.125F;
+				normals[i].y *= 0.125F;
+				normals[i].z *= 0.125F;
+			}
+
+			envmap_texture.drawtype = 2;
+			envmap_texture.flag = pTex->flag;
+			envmap_texture.tpage = envmap_sprite->tpage;
+			envmap_texture.u1 = normals[0].x + envmap_sprite->x1 + 0.125F;
+			envmap_texture.v1 = normals[0].y + envmap_sprite->y1 + 0.125F;
+			envmap_texture.u2 = normals[1].x + envmap_sprite->x1 + 0.125F;
+			envmap_texture.v2 = normals[1].y + envmap_sprite->y1 + 0.125F;
+			envmap_texture.u3 = normals[2].x + envmap_sprite->x1 + 0.125F;
+			envmap_texture.v3 = normals[2].y + envmap_sprite->y1 + 0.125F;
+		}
+
+		if (GlobalAlpha != 0xFF000000)
+			pTex->drawtype = 3;
+		
+		AddTriSorted(v, tri[0], tri[1], tri[2], pTex, 0);
+
+		if (envmap)
+		{
+			clrbak[0] = v[tri[0]].color;
+			clrbak[1] = v[tri[1]].color;
+			clrbak[2] = v[tri[2]].color;
+			spcbak[0] = v[tri[0]].specular;
+			spcbak[1] = v[tri[1]].specular;
+			spcbak[2] = v[tri[2]].specular;
+			RGB_M(v[tri[0]].color, num);
+			RGB_M(v[tri[1]].color, num);
+			RGB_M(v[tri[2]].color, num);
+			RGB_M(v[tri[0]].specular, num);
+			RGB_M(v[tri[1]].specular, num);
+			RGB_M(v[tri[2]].specular, num);
+			AddTriSorted(v, tri[0], tri[1], tri[2], &envmap_texture, 0);
+			v[tri[0]].color = clrbak[0];
+			v[tri[1]].color = clrbak[1];
+			v[tri[2]].color = clrbak[2];
+			v[tri[0]].specular = spcbak[0];
+			v[tri[1]].specular = spcbak[1];
+			v[tri[2]].specular = spcbak[2];
+		}
+
+		pTex->drawtype = drawbak;
+	}
+
+	DestVB->Unlock();
+}
+
 void inject_output(bool replace)
 {
 	INJECT(0x0047DA60, phd_PutPolygons, replace);
@@ -509,4 +714,5 @@ void inject_output(bool replace)
 	INJECT(0x0047F950, _InsertRoom, replace);
 	INJECT(0x00480570, RenderLoadPic, replace);
 	INJECT(0x0047D5B0, S_InitialisePolyList, replace);
+	INJECT(0x0047E8B0, phd_PutPolygonsPickup, replace);
 }
--- TOMB4/specific/output.h
+++ TOMB4/specific/output.h
@@ -10,6 +10,7 @@ void PrelightVerts(long nVerts, D3DTLVERTEX* v, MESH_DATA* mesh);
 void _InsertRoom(ROOM_INFO* r);
 void RenderLoadPic(long unused);
 void S_InitialisePolyList();
+void phd_PutPolygonsPickup(short* objptr, float x, float y, long color);
 
 #define S_GetObjectBounds	( (long(__cdecl*)(short*)) 0x0047FCF0 )
 #define CalcVertsColorSplitMMX ( (void(__cdecl*)(long, D3DTLVERTEX*)) 0x0047D810 )
@@ -17,6 +18,5 @@ void S_InitialisePolyList();
 #define StashSkinVertices	( (void(__cdecl*)(long)) 0x0047D6B0 )
 #define SkinVerticesToScratch	( (void(__cdecl*)(long)) 0x0047D760 )
 #define S_DumpScreen	( (long(__cdecl*)()) 0x0047FCA0 )
-#define phd_PutPolygonsPickup	( (void(__cdecl*)(short*, float, float, long)) 0x0047E8B0 )
 #define S_OutputPolyList	( (void(__cdecl*)()) 0x0047FA10 )
 #define phd_PutPolygonSkyMesh	( (void(__cdecl*)(short*, long)) 0x0047F620 )
