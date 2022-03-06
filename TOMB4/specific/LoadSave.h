1b56cdcfe5fb784536cb16e0f8cb3a508044ce6c
Troye <woopstombraider@gmail.com>
2022-03-05 23:30:53 -0600

    MemBltSurf;

--- TOMB4/specific/LoadSave.h
+++ TOMB4/specific/LoadSave.h
@@ -20,6 +20,7 @@ void S_DisplayMonoScreen();
 void CreateMonoScreen();
 void FreeMonoScreen();
 void RGBM_Mono(uchar* r, uchar* g, uchar* b);
+void MemBltSurf(void* dest, long x, long y, long w, long h, long dadd, void* source, long x2, long y2, DDSURFACEDESC2 surface, float xsize, float ysize);
 
 #define S_PauseMenu	( (long(__cdecl*)()) 0x0047CC60 )
 #define DoSlider	( (void(__cdecl*)(long, long, long, long, long, long, long, long)) 0x0047AB80 )
