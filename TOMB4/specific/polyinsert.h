621fe2364c6ba41b5e84f1ea7cd9b035b5d6bcbe
Troye <woopstombraider@gmail.com>
2022-03-14 13:29:20 -0500

    AddQuadClippedSorted;

--- TOMB4/specific/polyinsert.h
+++ TOMB4/specific/polyinsert.h
@@ -16,10 +16,10 @@ void InitialiseFogBulbs();
 void OmniEffect(D3DTLVERTEX* v);
 void OmniFog(D3DTLVERTEX* v);
 void AddTriClippedSorted(D3DTLVERTEX* v, short v0, short v1, short v2, TEXTURESTRUCT* tex, long double_sided);
+void AddQuadClippedSorted(D3DTLVERTEX* v, short v0, short v1, short v2, short v3, TEXTURESTRUCT* tex, long double_sided);
 
 #define CalcColorSplit	( (void(__cdecl*)(D3DCOLOR, D3DCOLOR*)) 0x00484A20 )
 #define InitialiseSortList	( (void(__cdecl*)()) 0x00481860 )
 #define SortPolyList	( (void(__cdecl*)(long, SORTLIST**)) 0x00481810 )
-#define AddQuadClippedSorted	( (void(__cdecl*)(D3DTLVERTEX*, short, short, short, short, TEXTURESTRUCT*, long)) 0x004842A0 )
 #define mD3DTransform	( (void(__cdecl*)(FVECTOR*, D3DMATRIX*)) 0x00481AE0 )
 #define AddClippedPoly	( (void(__cdecl*)(D3DTLBUMPVERTEX*, long, D3DTLBUMPVERTEX*, TEXTURESTRUCT*)) 0x00482E40 )
