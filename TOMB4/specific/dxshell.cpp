f6711c888db18daa54d6c1ff32f842edf4b18350
Troye <woopstombraider@gmail.com>
2022-04-18 11:51:26 +0300

    DXSetCooperativeLevel;

--- TOMB4/specific/dxshell.cpp
+++ TOMB4/specific/dxshell.cpp
@@ -2,6 +2,36 @@
 #include "dxshell.h"
 #include "function_stubs.h"
 
+long DDSCL_FLAGS[11] =	// for DXSetCooperativeLevel logging
+{
+	DDSCL_ALLOWMODEX,
+	DDSCL_ALLOWREBOOT,
+	DDSCL_CREATEDEVICEWINDOW,
+	DDSCL_EXCLUSIVE,
+	DDSCL_FPUSETUP,
+	DDSCL_FULLSCREEN,
+	DDSCL_MULTITHREADED,
+	DDSCL_NORMAL,
+	DDSCL_NOWINDOWCHANGES,
+	DDSCL_SETDEVICEWINDOW,
+	DDSCL_SETFOCUSWINDOW
+};
+
+const char* DDSCL_TEXT[11] =
+{
+	"allowmodex",
+	"allowreboot",
+	"createdevicewindow",
+	"exclusive",
+	"fpusetup",
+	"fullscreen",
+	"multithreaded",
+	"normal",
+	"nowindowchanges",
+	"setdevicewindow",
+	"setfocuswindow"
+};
+
 void DXBitMask2ShiftCnt(ulong mask, uchar* shift, uchar* count)
 {
 	uchar i;
@@ -94,6 +124,34 @@ long DXD3DCreate(LPDIRECTDRAW4 pDD4, void** pD3D)
 	return 1;
 }
 
+long DXSetCooperativeLevel(LPDIRECTDRAW4 pDD4, HWND hwnd, long flags)
+{
+	char* ptr;
+	char buf[1024];
+
+	strcpy(buf, "DXSetCooperativeLevel - ");
+	ptr = &buf[strlen(buf)];
+
+	for (int i = 0; i < 11; i++)
+	{
+		if (DDSCL_FLAGS[i] & flags)
+		{
+			strcpy(ptr, DDSCL_TEXT[i]);
+			ptr += strlen(DDSCL_TEXT[i]);
+			*ptr++ = ' ';
+			*ptr = '\0';
+		}
+	}
+
+	*(ptr - 1) = '\0';
+	Log(2, buf);
+
+	if (DXAttempt(pDD4->SetCooperativeLevel(hwnd, flags)) != DD_OK)
+		return 0;
+
+	return 1;
+}
+
 void inject_dxshell(bool replace)
 {
 	INJECT(0x00492240, DXBitMask2ShiftCnt, replace);
@@ -102,4 +160,5 @@ void inject_dxshell(bool replace)
 	INJECT(0x00491E50, AddStruct, replace);
 	INJECT(0x00491EA0, DXDDCreate, replace);
 	INJECT(0x00491F60, DXD3DCreate, replace);
+	INJECT(0x00492BE0, DXSetCooperativeLevel, replace);
 }
--- TOMB4/specific/dxshell.h
+++ TOMB4/specific/dxshell.h
@@ -9,6 +9,7 @@ long DXAttempt(HRESULT r);
 void* AddStruct(void* p, long num, long size);
 long DXDDCreate(LPGUID pGuid, void** pDD4);
 long DXD3DCreate(LPDIRECTDRAW4 pDD4, void** pD3D);
+long DXSetCooperativeLevel(LPDIRECTDRAW4 pDD4, HWND hwnd, long flags);
 
 #define DXGetErrorString	( (const char*(__cdecl*)(HRESULT)) 0x00490FF0 )
 #define DXSaveScreen	( (void(__cdecl*)(LPDIRECTDRAWSURFACE4, const char*)) 0x00494080 )
