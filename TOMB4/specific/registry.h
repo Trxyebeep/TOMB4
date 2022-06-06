372edf05c92b8dd1e1319b93cc45d2c49b069939
Troye <woopstombraider@gmail.com>
2022-06-06 17:54:11 -0500

    SaveSetup

--- TOMB4/specific/registry.h
+++ TOMB4/specific/registry.h
@@ -13,6 +13,7 @@ bool REG_ReadLong(char* SubKeyName, ulong& value, ulong defaultValue);
 bool REG_ReadBool(char* SubKeyName, bool& value, bool defaultValue);
 bool LoadSettings();
 void SaveSettings();
+bool SaveSetup(HWND hDlg);
 #ifdef GENERAL_FIXES
 bool REG_KeyWasCreated();
 #endif
