fcd798739c623989e097ddae465a5a1c13d12a02
Troye <woopstombraider@gmail.com>
2022-06-27 06:56:30 -0500

    XYUVGClipper

--- TOMB4/specific/clipping.h
+++ TOMB4/specific/clipping.h
@@ -5,5 +5,4 @@ void inject_clipping(bool replace);
 
 long ZClipper(long n, D3DTLBUMPVERTEX* in, D3DTLBUMPVERTEX* out);
 long visible_zclip(D3DTLVERTEX* v0, D3DTLVERTEX* v1, D3DTLVERTEX* v2);
-
-#define XYUVGClipper	( (long(__cdecl*)(long, D3DTLBUMPVERTEX*)) 0x0046E8D0 )
+long XYUVGClipper(long n, D3DTLBUMPVERTEX* in);
