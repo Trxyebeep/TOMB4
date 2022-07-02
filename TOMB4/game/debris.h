56b9197aa5a59039fe2eede7dfea563b09ff2b44
Troye <woopstombraider@gmail.com>
2022-07-02 10:16:10 -0500

    ShatterObject

--- TOMB4/game/debris.h
+++ TOMB4/game/debris.h
@@ -6,5 +6,4 @@ void inject_debris(bool replace);
 void UpdateDebris();
 void TriggerDebris(GAME_VECTOR* pos, void* TextInfo, short* Offsets, long* Vels, short rgb);
 long GetFreeDebris();
-
-#define ShatterObject	( (void(__cdecl*)(SHATTER_ITEM*, MESH_INFO*, short, short, long)) 0x00433410 )
+void ShatterObject(SHATTER_ITEM* ShatterItem, MESH_INFO* StaticMesh, short Num, short RoomNumber, long NoXZVel);
