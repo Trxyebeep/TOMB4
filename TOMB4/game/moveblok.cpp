be01fc52dc9fc1e7d3ac6677892dc38514644ade
Troye <woopstombraider@gmail.com>
2022-07-07 11:54:57 -0500

    TestBlockPush

--- TOMB4/game/moveblok.cpp
+++ TOMB4/game/moveblok.cpp
@@ -1,6 +1,8 @@
 #include "../tomb4/pch.h"
 #include "moveblok.h"
 #include "control.h"
+#include "collide.h"
+#include "objects.h"
 
 static void ClearMovableBlockSplitters(long x, long y, long z, short room_number)
 {
@@ -55,8 +57,89 @@ void InitialiseMovingBlock(short item_number)
 	ClearMovableBlockSplitters(item->pos.x_pos, item->pos.y_pos, item->pos.z_pos, item->room_number);
 }
 
+static long TestBlockPush(ITEM_INFO* item, long height, ushort quadrant)
+{
+	ITEM_INFO** itemlist;
+	ITEM_INFO* collided;
+	FLOOR_INFO* floor;
+	ROOM_INFO* r;
+	long x, y, z, rx, rz;
+	short room_number;
+
+	x = item->pos.x_pos;
+	y = item->pos.y_pos;
+	z = item->pos.z_pos;
+	itemlist = (ITEM_INFO**)&tsv_buffer[0];
+
+	switch (quadrant)
+	{
+	case NORTH:
+		z += 1024;
+		break;
+
+	case EAST:
+		x += 1024;
+		break;
+
+	case SOUTH:
+		z -= 1024;
+		break;
+
+	case WEST:
+		x -= 1024;
+		break;
+	}
+
+	room_number = item->room_number;
+	floor = GetFloor(x, y - 256, z, &room_number);
+	r = &room[room_number];
+	rx = (x - r->x) >> 10;
+	rz = (z - r->z) >> 10;
+
+	if (r->floor[rx * r->x_size + rz].stopper)
+		return 0;
+
+	if (GetHeight(floor, x, y - 256, z) != y)
+		return 0;
+
+	GetHeight(floor, x, y, z);
+
+	if (height_type != WALL)
+		return 0;
+
+	y -= height - 100;
+	floor = GetFloor(x, y, z, &room_number);
+
+	if (GetCeiling(floor, x, y, z) > y)
+		return 0;
+
+	rx = item->pos.x_pos;
+	rz = item->pos.z_pos;
+	item->pos.x_pos = x;
+	item->pos.z_pos = z;
+	GetCollidedObjects(item, 256, 1, itemlist, 0, 0);
+	item->pos.x_pos = rx;
+	item->pos.z_pos = rz;
+
+	if (itemlist[0])
+	{
+		for (int i = 0; itemlist[0] != 0; i++, itemlist++)
+		{
+			collided = itemlist[0];
+
+			if (collided->object_number == TWOBLOCK_PLATFORM || collided->object_number == HAMMER)
+				return 1;
+		}
+
+		return 0;
+	}
+
+	return 1;
+}
+
 void inject_moveblok(bool replace)
 {
 	INJECT(0x004094A0, ClearMovableBlockSplitters, replace);
 	INJECT(0x00409460, InitialiseMovingBlock, replace);
+	INJECT(0x00409B80, TestBlockPush, replace);
 }
