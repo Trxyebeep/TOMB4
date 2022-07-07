94bce104ec20fc1665e3da9a948d33ddf3ab063d
Troye <woopstombraider@gmail.com>
2022-07-07 09:35:57 -0500

    GetClimbTrigger

--- TOMB4/game/laraclmb.h
+++ TOMB4/game/laraclmb.h
@@ -14,6 +14,7 @@ void lara_as_climbend(ITEM_INFO* item, COLL_INFO* coll);
 void lara_col_climbstnc(ITEM_INFO* item, COLL_INFO* coll);
 void lara_col_climbing(ITEM_INFO* item, COLL_INFO* coll);
 void lara_col_climbdown(ITEM_INFO* item, COLL_INFO* coll);
+short GetClimbTrigger(long x, long y, long z, short room_number);
 
 #define LaraCheckForLetGo	( (long(__cdecl*)(ITEM_INFO*, COLL_INFO*)) 0x0042C980 )
 #define	LaraDoClimbLeftRight	( (void(__cdecl*)(ITEM_INFO*, COLL_INFO*, long, long)) 0x0042CD20 )
