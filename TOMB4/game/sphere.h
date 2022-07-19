e6ed2e5aa432089b9087cfa78113c4621c0c1ce7
Troye <woopstombraider@gmail.com>
2022-07-19 13:12:29 -0500

    GetJointAbsPosition

--- TOMB4/game/sphere.h
+++ TOMB4/game/sphere.h
@@ -6,5 +6,4 @@ void inject_sphere(bool replace);
 long GetSpheres(ITEM_INFO* item, SPHERE* ptr, long WorldSpace);
 long TestCollision(ITEM_INFO* item, ITEM_INFO* l);
 void InitInterpolate2(long frac, long rate);
-
-#define GetJointAbsPosition	( (void(__cdecl*)(ITEM_INFO*, PHD_VECTOR*, long)) 0x0045FF80 )
+void GetJointAbsPosition(ITEM_INFO* item, PHD_VECTOR* pos, long joint);
