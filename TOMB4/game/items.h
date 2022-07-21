32532620d00ac50495c9b881d1f5eabc89b432db
Troye <woopstombraider@gmail.com>
2022-07-21 00:33:38 -0500

    InitialiseItem

--- TOMB4/game/items.h
+++ TOMB4/game/items.h
@@ -6,6 +6,7 @@ void inject_items(bool replace);
 void InitialiseItemArray(short num);
 void KillItem(short item_num);
 short CreateItem();
+void InitialiseItem(short item_num);
 
 #define KillEffect	( (void(__cdecl*)(short)) 0x00454970 )
 #define RemoveActiveItem	( (void(__cdecl*)(short)) 0x004545F0 )
@@ -14,5 +15,4 @@ short CreateItem();
 #define CreateEffect	( (short(__cdecl*)(short)) 0x00454900 )
 #define InitialiseFXArray	( (void(__cdecl*)(long)) 0x004548B0 )
 #define AddActiveItem	( (void(__cdecl*)(short)) 0x00454740 )
-#define InitialiseItem	( (void(__cdecl*)(short)) 0x004543B0 )
 #define RemoveDrawnItem	( (void(__cdecl*)(short)) 0x004546A0 )
