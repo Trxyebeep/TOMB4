36730a839bfbbf29417dcc2b4a419339f7b05590
Troye <woopstombraider@gmail.com>
2022-07-22 23:27:34 -0500

    LaraSurface

--- TOMB4/game/larasurf.cpp
+++ TOMB4/game/larasurf.cpp
@@ -2,6 +2,11 @@
 #include "larasurf.h"
 #include "lara_states.h"
 #include "lara.h"
+#include "laraswim.h"
+#include "laramisc.h"
+#include "collide.h"
+#include "larafire.h"
+#include "control.h"
 
 void lara_as_surfswim(ITEM_INFO* item, COLL_INFO* coll)
 {
@@ -186,6 +191,54 @@ void lara_col_surftread(ITEM_INFO* item, COLL_INFO* coll)
 	LaraSurfaceCollision(item, coll);
 }
 
+void LaraSurface(ITEM_INFO* item, COLL_INFO* coll)
+{
+	camera.target_elevation = -4004;
+	coll->bad_pos = -NO_HEIGHT;
+	coll->bad_neg = -128;
+	coll->bad_ceiling = 100;
+	coll->old.x = item->pos.x_pos;
+	coll->old.y = item->pos.y_pos;
+	coll->old.z = item->pos.z_pos;
+	coll->radius = 100;
+	coll->trigger = 0;
+	coll->slopes_are_walls = 0;
+	coll->slopes_are_pits = 0;
+	coll->lava_is_pit = 0;
+	coll->enable_baddie_push = 0;
+	coll->enable_spaz = 0;
+
+	if (input & IN_LOOK && lara.look)
+		LookLeftRight();
+	else
+		ResetLook();
+
+	lara.look = 1;
+	lara_control_routines[item->current_anim_state](item, coll);
+
+	if (item->pos.z_rot >= -364 || item->pos.z_rot <= 364)
+		item->pos.z_rot = 0;
+	else if (item->pos.z_rot >= 0)
+		item->pos.z_rot -= 364;
+	else
+		item->pos.z_rot += 364;
+
+	if (lara.current_active && lara.water_status != LW_FLYCHEAT)
+		LaraWaterCurrent(coll);
+
+	AnimateLara(item);
+	item->pos.x_pos += item->fallspeed * phd_sin(lara.move_angle) >> (W2V_SHIFT + 2);
+	item->pos.z_pos += item->fallspeed * phd_cos(lara.move_angle) >> (W2V_SHIFT + 2);
+	LaraBaddieCollision(item, coll);
+
+	if (lara.vehicle == NO_ITEM)
+		lara_collision_routines[item->current_anim_state](item, coll);
+
+	UpdateLaraRoom(item, 100);
+	LaraGun();
+	TestTriggers(coll->trigger, 0, 0);
+}
+
 void inject_larasurf(bool replace)
 {
 	INJECT(0x00431C10, lara_as_surfswim, replace);
@@ -198,4 +251,5 @@ void inject_larasurf(bool replace)
 	INJECT(0x004322E0, lara_col_surfleft, replace);
 	INJECT(0x00432310, lara_col_surfright, replace);
 	INJECT(0x00432340, lara_col_surftread, replace);
+	INJECT(0x00431AA0, LaraSurface, replace);
 }
