cf74e5b6e71b1cf5823c57ea0f68d02f8adfed2b
Troye <woopstombraider@gmail.com>
2022-07-27 12:46:23 -0500

    DoFlareLight

--- TOMB4/game/laraflar.cpp
+++ TOMB4/game/laraflar.cpp
@@ -6,6 +6,8 @@
 #ifdef GENERAL_FIXES
 #include "draw.h"
 #endif
+#include "../specific/function_stubs.h"
+#include "effect2.h"
 
 void DrawFlareInAir(ITEM_INFO* item)
 {
@@ -48,9 +50,79 @@ void undraw_flare_meshes()
 	lara.mesh_ptrs[LM_LHAND] = meshes[objects[LARA].mesh_index + LM_LHAND * 2];
 }
 
+long DoFlareLight(PHD_VECTOR* pos, long flare_age)
+{
+	long x, y, z, r, g, b, rnd, falloff, ret;
+
+	if (flare_age >= 900 || !flare_age)
+		return 0;
+
+	rnd = GetRandomControl();
+	x = pos->x + ((rnd & 0xF) << 3);
+	y = pos->y + (rnd >> 1 & 120) - 256;
+	z = pos->z + (rnd >> 5 & 120);
+	ret = 1;
+
+	if (flare_age < 4)
+	{
+		falloff = (rnd & 3) + 4 + flare_age * 4;
+
+		if (falloff > 16)
+			falloff -= (rnd >> 12) & 3;
+
+		r = (rnd >> 4 & 0x1F) + 8 * flare_age + 32;
+		g = (rnd & 0x1F) + 16 * (flare_age + 10);
+		b = 16 * flare_age + ((rnd >> 8) & 0x1F);
+	}
+	else if (flare_age < 16)
+	{
+		falloff = (rnd & 1) + flare_age + 2;
+		r = ((rnd >> 4) & 0x1F) + 4 * flare_age + 64;
+		g = (rnd & 0x3F) + 4 * flare_age + 128;
+		b = ((rnd >> 8) & 0x1F) + 4 * flare_age + 16;
+	}
+	else if (flare_age < 810)
+	{
+		falloff = 16;
+		r = (rnd >> 4 & 0x1F) + 128;
+		g = (rnd & 0x3F) + 192;
+		b = ((rnd >> 8) & 0x3F) + (((rnd >> 6) & 0x7F) >> 2);
+	}
+	else if (flare_age < 876)
+	{
+		if (rnd > 0x2000)
+		{
+			falloff = 16;
+			r = (rnd & 0x1F) + 128;
+			g = (rnd & 0x3F) + 192;
+			b = ((rnd >> 8) & 0x3F) + (((rnd >> 6) & 0x7F) >> 2);
+		}
+		else
+		{
+			r = (GetRandomControl() & 0x3F) + 64;
+			g = (GetRandomControl() & 0x3F) + 192;
+			b = GetRandomControl() & 0x7F;
+			falloff = (GetRandomControl() & 6) + 8;
+			ret = 0;
+		}
+	}
+	else
+	{
+		falloff = 16 - ((flare_age - 876) >> 1);
+		r = (GetRandomControl() & 0x3F) + 64;
+		g = (GetRandomControl() & 0x3F) + 192;
+		b = GetRandomControl() & 0x1F;
+		ret = rnd & 1;
+	}
+
+	TriggerDynamic(x, y, z, falloff, r, g, b);
+	return ret;
+}
+
 void inject_laraflar(bool replace)
 {
 	INJECT(0x0042F7B0, DrawFlareInAir, replace);
 	INJECT(0x0042FF10, draw_flare_meshes, replace);
 	INJECT(0x0042FF30, undraw_flare_meshes, replace);
+	INJECT(0x0042F510, DoFlareLight, replace);
 }
--- TOMB4/game/laraflar.h
+++ TOMB4/game/laraflar.h
@@ -6,5 +6,6 @@ void inject_laraflar(bool replace);
 void DrawFlareInAir(ITEM_INFO* item);
 void draw_flare_meshes();
 void undraw_flare_meshes();
+long DoFlareLight(PHD_VECTOR* pos, long flare_age);
 
 #define CreateFlare	( (void(__cdecl*)(short, long)) 0x0042F880 )
