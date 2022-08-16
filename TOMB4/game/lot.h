d687b3f474ad23b0a836c7f086a6c3ad191b3f0b
Troye <woopstombraider@gmail.com>
2022-08-16 04:59:00 -0500

    EnableBaddieAI

--- TOMB4/game/lot.h
+++ TOMB4/game/lot.h
@@ -8,4 +8,4 @@ void DisableBaddieAI(short item_number);
 void ClearLOT(LOT_INFO* lot);
 void CreateZone(ITEM_INFO* item);
 void InitialiseSlot(short item_number, long slot);
-#define EnableBaddieAI	( (long(__cdecl*)(short, long)) 0x004552B0 )
+long EnableBaddieAI(short item_number, long Always);
